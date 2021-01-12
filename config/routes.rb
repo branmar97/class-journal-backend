e5e0a529908bf7023c29430434a87f232c1d7434
branmar97 <brandonmar97@gmail.com>
2021-01-11 20:35:34 -0500

    Remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :entries
+  resources :comments, only: [:index, :create]
+  resources :entries, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
